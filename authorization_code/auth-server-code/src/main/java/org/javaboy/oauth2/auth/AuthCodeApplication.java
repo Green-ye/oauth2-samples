3c6a89a9ea9ec5efd50dd82489c71a1fa942410d
yezhenghai <15797655755@163.com>
2023-05-26 20:23:28 +0800

    提交code模块代码

---
+++ authorization_code/auth-server-code/src/main/java/org/javaboy/oauth2/auth/AuthCodeApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.oauth2.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCodeApplication.class, args);
+    }
+
+}
