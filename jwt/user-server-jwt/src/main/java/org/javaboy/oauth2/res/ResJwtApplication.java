eeeea3eda0c75297a9d72c5edb0fc9cd76b62131
yezhenghai <15797655755@163.com>
2023-05-26 20:13:09 +0800

    提交jwt模块代码

---
+++ jwt/user-server-jwt/src/main/java/org/javaboy/oauth2/res/ResJwtApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.oauth2.res;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResJwtApplication.class, args);
+    }
+
+}
